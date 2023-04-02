7a3c838cc13f62d9dd4e76105b57ebeb35775fb9
RohitGhorai <98656284+RohitGhorai@users.noreply.github.com>
2023-04-03 01:19:15 +0530

    profile info page

--- src/user-routes/ProfileInfo.jsx
+++ src/user-routes/ProfileInfo.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import Base from '../components/Base'
+import React from "react";
+import Base from "../components/Base";
+
+import { useState } from "react";
+import { useEffect } from "react";
+import { getSingleUser } from "../services/user-service";
+import { useParams } from "react-router-dom";
+import ViewUserProfile from "../components/ViewUserProfile";
 
 const ProfileInfo = () => {
+  const { userId } = useParams();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    getSingleUser(userId).then((data) => {
+      setUser(data);
+      console.log(data);
+    });
+  }, []);
   return (
     <Base>
-      <h1>Profile page.</h1>
+      <ViewUserProfile user={user} />
     </Base>
-  )
-}
+  );
+};
 
-export default ProfileInfo
+export default ProfileInfo;
